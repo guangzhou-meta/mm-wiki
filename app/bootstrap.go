7460fee96038df1f9d63e4d55ce28f8fd55e33be
Changeden <changeden520@gmail.com>
2022-04-20 18:01:56 +0800

    support search documents by keyword for content

--- app/bootstrap.go
+++ app/bootstrap.go
@@ -95,7 +95,7 @@ func initConfig() {
 		log.Println("init config error: " + err.Error())
 		os.Exit(1)
 	}
-	RootDir = "./"
+	//RootDir = "./"
 	confFile := *confPath
 	if *confPath == "" {
 		confFile = filepath.Join(RootDir, defaultConf)
